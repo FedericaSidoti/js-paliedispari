718176986afc41b4bd367fa64c2194e810a80987
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-12 18:43:29 +0200

    ADD- functions sum and isEven, correct return

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ const wordReverse = arrayLetters.toString();
 const wordStraight = arrayLettersreverse.toString();
 
 // creare una funzione per verificare che la parola sia (vero) o non sia (falso) palindroma
-function isPalindrome (word, wordReverse) {
-    if (word === wordReverse) {
+function isPalindrome (word, wordreverse) {
+    if (word === wordreverse) {
         return true;
     } else {
         return false; 
@@ -33,22 +33,50 @@ if (isPalindrome(wordStraight, wordReverse)) {
     alert("questa parola non è palindroma")
 }
 
+/////SECONDO ESERCIZIO///////
+
 //- chiedere all' utente se pari o dispari 
 const teamRequest = prompt('scrivi PARI o DISPARI')
 //- chiedere all'utente un numero da 1 a 5 
-const numRequest = prompt ('inserisci un numero da 1 a 5')
-    if (numRequest === NaN || numRequest > 5) {
+const numRequest = prompt('inserisci un numero da 1 a 5')
+    if (numRequest === NaN || numRequest > 5 || numRequest < 1) {
         alert('il valore inserito non è valido') 
     }
-
+const userNumber = parseInt(numRequest);
 //- scrivere una funzione che generi un numero random da 1 a 5
-//    -salvare il return in una variabile numComputer
+let pcNumber = 0;
+function generateRandomPCNumber(min, max) {
+    pcNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return pcNumber; 
+}
+//- salvare il return in una variabile pcNumber
+pcNumber = generateRandomPCNumber(1, 5)
+console.log(pcNumber)
 //- scrivere una funzione somma 
-//    - salvare il return in una variabile
+let sum = 0;
+function sumTwoNumbers (num1, num2) {
+    sum = num1 + num2
+    return sum
+}
+//- salvare il return in una variabile
+sum = sumTwoNumbers (userNumber, pcNumber)
+console.log(sum)
 //- scrivere una funzione che verifichi se il numero è pari (true)
-//- invocare la funzione somma con i parametri corretti
+function isEven (num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false; 
+    }
+}
 //- SE funzione = pari e prompt = pari OPPURE se funzione= nonpari e prompt = dispari
+if (isEven(sum) === true && teamRequest === 'PARI' || isEven(sum) === false && teamRequest === 'DISPARI'){
+    alert('HAI VINTO!')
+} else {
+    alert('HAI PERSO!')
+}
 //    - alert "hai vinto"
+
 //- ALTRIMENTI 
 //    - alert hai perso 
 
