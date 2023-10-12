8cfb855c3adeee9cb6471c4d4297a13ab843cc49
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-12 17:38:03 +0200

    ADD- prompt with controls on requested number

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
-- chiedere all' utente se pari o dispari 
+- chiedere all'utente se pari o dispari 
 - chiedere all'utente un numero da 1 a 5 
 - scrivere una funzione che generi un numero random da 1 a 5
     -salvare il return in una variabile numComputer
--- js/app.js
+++ js/app.js
@@ -33,3 +33,22 @@ if (isPalindrome(wordStraight, wordReverse)) {
     alert("questa parola non è palindroma")
 }
 
+//- chiedere all' utente se pari o dispari 
+const teamRequest = prompt('scrivi PARI o DISPARI')
+//- chiedere all'utente un numero da 1 a 5 
+const numRequest = prompt ('inserisci un numero da 1 a 5')
+    if (numRequest === NaN || numRequest > 5) {
+        alert('il valore inserito non è valido') 
+    }
+
+//- scrivere una funzione che generi un numero random da 1 a 5
+//    -salvare il return in una variabile numComputer
+//- scrivere una funzione somma 
+//    - salvare il return in una variabile
+//- scrivere una funzione che verifichi se il numero è pari (true)
+//- invocare la funzione somma con i parametri corretti
+//- SE funzione = pari e prompt = pari OPPURE se funzione= nonpari e prompt = dispari
+//    - alert "hai vinto"
+//- ALTRIMENTI 
+//    - alert hai perso 
+
